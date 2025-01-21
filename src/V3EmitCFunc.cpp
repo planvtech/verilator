cad6eaee7c09ea970cfdf6ee4cde27aa07f6ea12
Yilou Wang <wangyilou123@gmail.com>
2025-01-21 14:33:32 +0100

    Try with $

--- src/V3EmitCFunc.cpp
+++ src/V3EmitCFunc.cpp
@@ -361,6 +361,9 @@ void EmitCFunc::displayNode(AstNode* nodep, AstScopeName* scopenamep, const stri
             case '@':
                 displayArg(nodep, &elistp, isScan, vfmt, ignore, '@');
                 break;  // Packed string
+            case '$':
+                displayArg(nodep, &elistp, isScan, vfmt, ignore, '$');
+                break;
             // Spec: h d o b c l
             case 'b': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'b'); break;
             case 'c': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'c'); break;
@@ -371,7 +374,6 @@ void EmitCFunc::displayNode(AstNode* nodep, AstScopeName* scopenamep, const stri
             case 'o': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'o'); break;
             case 'h':  // FALLTHRU
             case 'x': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'x'); break;
-            case 'p': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'p'); break;
             case 's': displayArg(nodep, &elistp, isScan, vfmt, ignore, 's'); break;
             case 'e': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'e'); break;
             case 'f': displayArg(nodep, &elistp, isScan, vfmt, ignore, 'f'); break;
